f7a32afb2135c84e9eee8f04ec62ddb41a246f4a
VaheMeekz <vahe.mkrtchyan.a3@gmial.com>
2022-06-07 09:36:21 +0400

    sync tests

--- sync/sync.js
+++ sync/sync.js
@@ -4,7 +4,14 @@ class Lodash{
     }
 
     groupBy(array, prop){
-
+        return array.reduce((acc,i)=>{
+            const key = typeof prop == "function" ? prop(i) : i[prop]
+            if(!acc[key]){
+                acc[key] = []
+            }
+            acc[key].push(i)
+            return acc;
+        },{})
     }
 }
 
--- sync/sync.test.js
+++ sync/sync.test.js
@@ -1,10 +1,11 @@
 const Lodash = require('./sync')
-
+let _ = new Lodash()
 describe("Lodash: ", () => {
 
-    const _ = new Lodash()
 
-
+    afterAll(()=>{
+        _ = new Lodash()
+    })
     test("should be defined", () => {
         expect(_.compact).toBeDefined()
         expect(_.compact).not.toBeUndefined()
@@ -22,5 +23,27 @@ describe("Lodash: ", () => {
         expect(_.compact(array)).not.toContain(null)
         expect(_.compact(array)).not.toContain(false)
         expect(_.compact(array)).not.toContain("")
+    })
+})
+
+describe("Lodash group by: ",()=>{
+    test("should be defined", () => {
+        expect(_.groupBy).toBeDefined()
+        expect(_.groupBy).not.toBeUndefined()
+    })
+
+    test("should by array items by Math.floor",()=>{
+        // const array = [2.2,2.4,4.2,3.1]
+        // const result = {
+        //     2:[2.2,2.4],
+        //     4:[4.2],
+        //     3:[3.1]
+        // }
+        const array = ["one","two","three"]
+        const result = {
+            5:["three"],
+            3:["one","two"]
+        }
+        expect(_.groupBy(array,'length')).toEqual(result)
     })
 })
