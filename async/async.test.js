dae1ab733975251474a90fa3f9eb387c4ec3fed0
VaheMeekz <vahe.mkrtchyan.a3@gmial.com>
2022-06-08 15:31:17 +0400

    async tests part two

--- async/async.test.js
+++ async/async.test.js
@@ -1,8 +1,21 @@
 const Async = require("./async")
 
 
-describe("Async echo: ",()=>{
-        test("should returned value",async ()=>{
-            const response = await Async.echo("data")
+describe("Async echo: ", () => {
+    test("should returned value", async () => {
+        const response = await Async.echo("data")
+        expect(response).toBe('data')
+    })
+
+    test("should returned value", async () => {
+        return Async.echo("data").then(data => {
+            expect(data).toBe('data')
         })
+    })
+
+    test("should returned value", async () => {
+        return Async.echo().catch(err => {
+            expect(err).toBeInstanceOf(Error)
+        })
+    })
 })
